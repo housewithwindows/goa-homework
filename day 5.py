7db18cd44acc8e03d40e9e8ce7f4c9a9d4e12b7a
housewithwindows <tsikoridzenino1963@gmail.com>
2024-02-22 22:16:50 +0400

    commit

---
+++ day 5.py
@@ -0,0 +1,15 @@
+academy="GOA"
+print(academy)
+      # me aq shevqmeni string tipis cvladi saxelad academy da vcade daprintva.mesijma amoagdo erori radgan a brjyali zedmetia.print("academy")
+print(budget)
+budget=1
+#me vcade budgetis daprintva romlis mnishvneloba aris 1.erori amoagdo radgan jer davprinte da mere davadeklarire.budget=1 print(budget)
+budget=15
+print(budget
+# me davadeklarire cvladi saxelad budget xolo mere daprintva vcade magram error amoagdo radgan brchxili aklia.print (budget)
+print(5 + "5") 
+# me vcade rom mimematebina int 5 str 5ze magram error daawera radgan stringi integers ar emateba.print(5+5)
+print("god of wars)
+# me vcade dameprinta string tipis sityvas god of wars magram error daawera radgan brjyali aklia. print("god of wars")     
+     
+      
