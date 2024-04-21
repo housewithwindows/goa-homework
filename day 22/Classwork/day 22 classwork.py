c14ab0bc9f13de56faf3cdef9fca38bad113d666
housewithwindows <tsikoridzenino1963@gmail.com>
2024-04-21 22:08:08 +0400

    commit

---
+++ day 22/Classwork/day 22 classwork.py
@@ -0,0 +1,6 @@
+# Vicit romelicaa sauketeso akademia mara mainc vkitxot VS Codes.
+i="goa"
+while input("Sauketeso akademia?:   ") != i:    
+    input("Sauketeso akademia?:   ")   
+else:
+    print("Pasuxi Ra Tqma Unda Sworia")
---
+++ day 22/Homework/day 22 homework.py
@@ -0,0 +1,9 @@
+# day 22 is davalebaa es.
+# exla me gavaketeb while loop-ის magalits.
+
+
+Academy="goa"
+while input("Which Academy Is The Best?:   ") != Academy:
+      print("you are wrong")
+else:
+    print("Finally,You Got it Correct!")
